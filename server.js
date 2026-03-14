0799ab52c287f94b44f5cdc9aa99b58d96c905d2
Rishi Raghuvanshi <rishisrmscet@gmail.com>
2026-03-14 18:31:14 +0530

    DataBase Connected to server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./src/config/db");
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT,()=>console.log(`Sever is running at PORT ${PORT}`))
+connectDB();
+
+app.use(cors());
+app.use(bodyParser);
+app.listen(PORT, () => console.log(`Sever is running at PORT ${PORT}`));
